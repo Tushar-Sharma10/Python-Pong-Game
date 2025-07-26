52e4966dc09c0de0f4925070c3a189c08505f158
Tushar Sharma <tusharsharma.techsavvy@gmail.com>
2025-07-26 15:19:37 -0400

    Set up the main screen

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import Screen
+
+# SET UP THE MAIN SCREEN
+screen = Screen()
+screen.title("Pong Game")
+screen.setup(width=800,height=600)
+screen.bgcolor("Black")
+
+
+screen.exitonclick()
