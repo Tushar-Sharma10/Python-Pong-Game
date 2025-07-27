ce817050e626935e7e3f502eef0005d94f214be9
Tushar Sharma <tusharsharma.techsavvy@gmail.com>
2025-07-26 22:13:56 -0400

    Initialize paddle objects and assign keyboard listeners

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from turtle import Screen
+from paddle import Paddle
 
 # SET UP THE MAIN SCREEN
 screen = Screen()
+screen.tracer(0)
 screen.title("Pong Game")
 screen.setup(width=800,height=600)
 screen.bgcolor("Black")
 
+# CREATE 2 PADDLES
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+
+screen.listen()
+#RIGHT PADDLE LISTENERS
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down,"Down")
+
+# LEFT PADDLE LISTENERS
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down,"s")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
 
 screen.exitonclick()
